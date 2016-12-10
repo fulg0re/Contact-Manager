b68483086d20602fff441898c9e04a8b99da3dc6
fulg0re <fulg0re.den@gmail.com>
2016-12-10 14:55:52 +0200

    Code: main menu use <ul> <li>

--- www/OOP/App/Views/Elements/header.php
+++ www/OOP/App/Views/Elements/header.php
@@ -4,33 +4,31 @@
     </a>
 
     <?php if ($_SESSION['logined'] == true): ?>
-        <div id="control-panel">
-            <div class="control-buttons" id="home-button">
+        <ul id="control-panel">
+            <li class="control-buttons" id="home-button">
                 <a href='/contacts'>
                     <div class="control-buttons-content">
                         <img id="home-img">
                         <p id="home-p">Home</p>
                     </div>
                 </a>
-            </div>
-
-            <div class="control-buttons" id="selection-button">
+            </li>
+            <li class="control-buttons" id="selection-button">
                 <a href='/contacts/selection'>
                     <div class="control-buttons-content">
                         <img id="selection-img">
                         <p id="selection-p">Selection page</p>
                     </div>
                 </a>
-            </div>
-
-            <div class="control-buttons" id="logout-button">
+            </li>
+            <li class="control-buttons" id="logout-button">
                 <a href='/contacts/logout'>
                     <div class="control-buttons-content">
                         <img id="logout-img">
                         <p id="auth-p">Logout</p>
                     </div>
                 </a>
-            </div>
-        </div>
+            </li>
+        </ul>
     <?php endif; ?>
 </div>
