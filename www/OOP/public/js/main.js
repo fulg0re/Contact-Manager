da92b2ec44c2b73f771b53810d0cc45bb7748153
fulg0re <fulg0re.den@gmail.com>
2017-05-08 16:34:47 +0300

    Code: change some js

--- www/OOP/public/js/main.js
+++ www/OOP/public/js/main.js
@@ -1,5 +1,5 @@
 function AlertIt(id) {
-    var answer = confirm ("Do you realy want to delete record with id: " + id)
+    var answer = confirm ("Do you really want to delete record with id: " + id)
     if (answer){
         if (typeof (id) == "number"){
             window.location="/contacts/delete/" + id;
